bdcf7d6958a89fcb30a52a0eca482ecbe65f9639
Nathan S <natinusala@gmail.com>
2016-09-28 12:00:57 +0200

    ajout de fonctions dans File

--- src/khanar-common/file/File.cpp
+++ src/khanar-common/file/File.cpp
@@ -8,28 +8,35 @@
 
 namespace khanar
 {
-      //TODO Vérifier si les paramètres ne sont pas vides
-
       File::File(File* parent, string name)
       {
-        //TODO Vérifier que le parent est un dossier
-        this->_name = name;
+        //TODO Vérifier que le parent est un dossier et existe dans la condition
 
-        if (parent != NULL)
+        if (parent == NULL)
         {
-          this->_parentFolderAbsolutePath = parent->getAbsolutePath();
+          //TODO Throw exception
         }
         else
         {
-          this->_parentFolderAbsolutePath = "";
+          File(parent->getAbsolutePath() + "/" + name);
         }
       }
 
       File::File(string absolutepath)
       {
+        if (absolutepath.empty())
+        {
+          absolutepath = "/";
+        }
+
         size_t pos = absolutepath.find_last_of("/");
         this->_name = absolutepath.substr(pos+1);
         this->_parentFolderAbsolutePath = absolutepath.substr(0, pos);
+
+        this->_absolutePath = absolutepath;
+
+        this->_fileStat = (const struct stat) {0};
+        this->_exists = stat(this->getAbsolutePath().c_str(), &this->_fileStat);
       }
 
       string File::getName()
@@ -39,11 +46,26 @@ namespace khanar
 
       string File::getAbsolutePath()
       {
-        return this->_parentFolderAbsolutePath + "/" + this->_name;
+        return this->_absolutePath;
       }
 
       string File::getParentFolderAbsolutePath()
       {
         return this->_parentFolderAbsolutePath;
+      }
+
+      bool File::exists()
+      {
+        return this->_exists;
+      }
+
+      bool File::isDirectory()
+      {
+        return this->_fileStat.st_mode & S_IFDIR;
+      }
+
+      bool File::isHidden()
+      {
+        return this->_name[0] == '.';
       }
 }
