6ff52520b319c869a8120150b930e065a376adf6
Killy <goureau.n.a@hotmail.fr>
2016-10-05 17:39:44 +0200

    J'ai des gros doigts

--- src/khanar.cpp
+++ src/khanar.cpp
@@ -10,6 +10,7 @@
 
 #include "compiled_assets/window.glade.hex"
 #include "compiled_assets/topbar.glade.hex"
+#include "compiled_assets/boxlayout.glade.hex"
 
 using namespace std;
 using namespace khanar;
@@ -23,33 +24,40 @@ int main(int argc, char* argv[])
 
   try
 	{
-
-		
+//Création des constructeurs glade
 		RefPtr<Builder> win_builder = Assets::buildGtkFromResource(window_glade);
+    RefPtr<Builder> topbar_builder = Assets::buildGtkFromResource(topbar_glade);
+    RefPtr<Builder> box_builder = Assets::buildGtkFromResource(boxlayout_glade);
+
 
 		Window* win = nullptr;
-		win_builder->get_widget("window1", win);
+
+    //Initialisation de la fenêtre
+    win_builder->get_widget("window1", win);
 
     Notebook* notebook = nullptr;
+    //Initialisation de la boite a onglet
 		win_builder->get_widget("notebook1", notebook);
     Box* box_window = nullptr;
+    //Initialisation de la boite conteneur
 		win_builder->get_widget("box1", box_window);
-    Box box_window2 = Box();
-    RefPtr<Builder> topbar_builder = Assets::buildGtkFromResource(topbar_glade);
+    Box* box_window2 = nullptr;
+    //Initialisation de la boite conteneur test
+    box_builder->get_widget("box1", bow_window2);
+
 
 	Label label = Label("Mdrcopter");
 
     Box* topbarbox = nullptr;
     topbar_builder->get_widget("box1", topbarbox);
-    
-    notebook->remove_page(0);
-    notebook->append_page(*box_window, "lolMdr Khanar Fdp");
-    box_window2.pack_start(label,true, true);
 
-    notebook->append_page(box_window2, "Mange tes morts");
+    //notebook->remove_page(0);
+    notebook->append_page(*box_window, "lolMdr Khanar Fdp");
+
+    notebook->append_page(*box_window2, "Mange tes morts");
 
     box_window->pack_start(*topbarbox,true, true);
- 
+
 
 
 		Main::run(*win);
