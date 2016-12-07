7b661a2db0b7a75a2c630edaeb54f86a2a644c74
Nathan S <natinusala@gmail.com>
2016-12-08 00:18:19 +0100

    encore de la doc

--- src/gui/widget/WidgetPerso.hpp
+++ src/gui/widget/WidgetPerso.hpp
@@ -5,13 +5,19 @@
 
 namespace khanar
 {
+  /**
+  * \class WidgetPerso
+  *
+  * Abstraction d'un widget
+  * Peut être une classe maison ou un widget Gtk
+  */
   class WidgetPerso
   {
   protected:
     void* content; ///< Contenu du WidgetPerso (peut être une classe maison ou un widget Gtk)
 
   public:
-    void* getContent();
+    void* getContent(); ///< Renvoie le contenu du widget
   };
 
   class GladeWidget : public WidgetPerso
